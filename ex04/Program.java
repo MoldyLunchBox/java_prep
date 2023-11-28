7310e9d4006191b3de302393d0e89fccd5255861
MoldyLunchBox <duu116@gmail.com>
2023-11-28 13:13:41 +0100

    update

--- ex04/Program.java
+++ ex04/Program.java
@@ -6,20 +6,59 @@ import java.util.Map;
 
 public class Program {
     public static void main(String[] args) {
-        String str = "aaaaaaabbbbbbssssssaaaaa";
-        char[][] stats = new char[127][2];
-
-        int max = Integer.MIN_VALUE;
+        String str = "aaaaaaabbbbbbbbbbbbbbbsssssssaaaaa";
+        int[][] stats = new int[65536][2];
+        int histogramLength = 0;
+        String histogramElements = "";
+        String histoLine = "";
         for (Character c : str.toCharArray()) {
-            stats[(int) c][0] = c;
+            stats[(int) c][0] = (int) c;
             stats[(int) c][1]++;
-
         }
+        int[][] sortedArr = sortArray(stats, str.length());
+        histogramLength = histogramLength == 0 ? sortedArr[0][1] : histogramLength;
+        for (int i = 0; i < histogramLength; i++) {
+            histoLine = "";
+            for (int[] index : sortedArr) {
+                if (index[1] >= histogramLength - i) {
+                    if (histogramLength - i == index[1])
+                        histoLine += index[0] + "  ";
+                    else
+                        histoLine += "#  ";
 
-        for(char [] index: stats){
-            System.out.println(index[0] + "=" + index[1] );
+                }
+                if (i == 0 && index[1] > 0) {
+                    histogramElements += (char)index[0] + "  ";
+                }
+                
+            }
+            System.out.println(histoLine);
         }
+        System.out.println(histogramElements);
+    }
 
+    public static int[][] sortArray(int[][] arr, int size) {
+        int[][] ret = new int[size][2];
+        int i = 0;
+        for (int[] index : arr) {
+            if (index[1] > 0) {
+                if (i == 0) {
+                    ret[i][0] = index[0];
+                    ret[i][1] = index[1];
+                } else {
+                    if (index[1] > ret[i - 1][1]) {
+                        int[] tmp = new int[2];
+                        tmp = ret[i - 1];
+                        ret[i - 1] = index;
+                        ret[i] = tmp;
+                    } else
+                        ret[i] = index;
+                }
+                i++;
+
+            }
+        }
+        return ret;
     }
 
     public static String[] printMap(Map<Character, Integer> mymap, Integer max) {
@@ -27,10 +66,10 @@ public class Program {
         int i = 0;
         for (Map.Entry<Character, Integer> entry : mymap.entrySet()) {
             System.out.println(entry.getKey() + ":" + entry.getValue() + "*".repeat(entry.getValue()));
-            arr[i] = "*".repeat(entry.getValue()) +"entry.getValue()" + " ".repeat(max - entry.getValue()) ;
+            arr[i] = "*".repeat(entry.getValue()) + "entry.getValue()" + " ".repeat(max - entry.getValue());
             i++;
         }
         return arr;
     }
 
-}************************
+}
